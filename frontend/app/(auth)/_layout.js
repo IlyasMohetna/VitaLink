af82bb183a42a517881f6057f3b6f99fe41cbbac
ilyas mohetna <ilyas.mohetna@hotmail.com>
2024-03-26 20:47:14 +0100

    fix bugs

--- frontend/app/(auth)/_layout.js
+++ frontend/app/(auth)/_layout.js
@@ -1,14 +1,39 @@
 // navigators/AuthStackNavigator.js
-import React from 'react';
+import React from "react";
 import { Stack } from "expo-router";
 
-export default function RootLayout(){
-  return(
+export default function RootLayout() {
+  return (
     <Stack>
-        <Stack.Screen name="landing" options={{headerShown: false}}/>
-        <Stack.Screen name="login" options={{headerShown: false, headerTitle: "Connectez-vous",headerBackVisible: false}}/>
-        <Stack.Screen name="code_signup" options={{headerShown: true, headerTitle: "", headerBackVisible: true, headerTintColor: "black", headerBackTitle: "Retour à l'introduction" }}/>
-        <Stack.Screen name="signup" options={{headerShown: true, headerTitle: "", headerBackVisible: true, headerTintColor: "black", headerBackTitle: "Retour" }}/>
+      <Stack.Screen name="landing" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="login"
+        options={{
+          headerShown: false,
+          headerTitle: "Connectez-vous",
+          headerBackVisible: false,
+        }}
+      />
+      <Stack.Screen
+        name="code_signup"
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerBackVisible: true,
+          headerTintColor: "black",
+          headerBackTitle: "Retour à l'introduction",
+        }}
+      />
+      <Stack.Screen
+        name="signup"
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerBackVisible: true,
+          headerTintColor: "black",
+          headerBackTitle: "Retour",
+        }}
+      />
     </Stack>
-  )
+  );
 }
--- frontend/app/_layout.js
+++ frontend/app/_layout.js
@@ -1,14 +1,14 @@
 // navigators/AuthStackNavigator.js
-import React from 'react';
+import React from "react";
 import { Slot, Stack } from "expo-router";
-import { AuthProvider } from '../context/AuthProvider';
-import Root from './Root';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { AuthProvider } from "../context/AuthProvider";
+import Root from "./Root";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function RootLayout(){  
-  return(
-      <AuthProvider>
-          <Root />
-      </AuthProvider>
-  )
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <Root />
+    </AuthProvider>
+  );
 }
