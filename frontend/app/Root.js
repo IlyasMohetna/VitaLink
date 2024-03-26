ef606376748260eaaecf489d7ec5ed8b15d40eb7
ilyas mohetna <ilyas.mohetna@hotmail.com>
2024-03-26 09:35:16 +0100

    block returning back gesture

--- frontend/app/Root.js
+++ frontend/app/Root.js
@@ -1,32 +1,38 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Stack } from 'expo-router'
-import { AuthContext } from '../context/AuthProvider';
-import * as SecureStore from 'expo-secure-store';
-import { router } from 'expo-router';
+import React, { useContext, useEffect, useState } from "react";
+import { Stack } from "expo-router";
+import { AuthContext } from "../context/AuthProvider";
+import * as SecureStore from "expo-secure-store";
+import { router } from "expo-router";
 
 const Root = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const { user, setUser } = useContext(AuthContext);
-  
-    useEffect(() => {
-      SecureStore.getItemAsync("user")
-        .then((userString) => {
-          if (userString) {
-            setUser(JSON.parse(userString));
-          }
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          setIsLoading(false);
-        });
-    }, []);
+  const [isLoading, setIsLoading] = useState(true);
+  const { user, setUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    SecureStore.getItemAsync("user")
+      .then((userString) => {
+        if (userString) {
+          setUser(JSON.parse(userString));
+        }
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <Stack>
-        <Stack.Screen name="(patient)" options={{headerShown: false}}/>
-        <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+      <Stack.Screen
+        name="(patient)"
+        options={{ headerShown: false, gestureEnabled: false }}
+      />
+      <Stack.Screen
+        name="(auth)"
+        options={{ headerShown: false, gestureEnabled: false }}
+      />
     </Stack>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
